e19f97a324218f81b977f49ab39f49c34a99bdf5
fuz3Tilt <kradinnikita@yandex.ru>
2023-08-25 16:45:28 +0300

    Added some comments

--- src/main/java/ru/kradin/social_media/services/interfaces/MessageService.java
+++ src/main/java/ru/kradin/social_media/services/interfaces/MessageService.java
@@ -7,6 +7,12 @@ import ru.kradin.social_media.exceptions.NotFoundException;
 import ru.kradin.social_media.models.Message;
 
 public interface MessageService {
+    /**
+     * Return messages with user
+     * @param secondUserId
+     * @param pageable
+     * @return
+     */
     Page<Message> getBySecondUserId(long secondUserId, Pageable pageable);
     void write(String text, long recipientId) throws NotFoundException;
 }
